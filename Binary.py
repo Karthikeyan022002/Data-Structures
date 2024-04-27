e0eb0d452af36e7d6d477fa538b9769b7feb5b72
Karthikeyan R1 <karthikeyan.t6635@ltimindtree.com>
2024-04-27 10:28:28 +0530

    added types of binary tree

--- Binary.py
+++ Binary.py
@@ -1,2 +1,8 @@
 # new data structure using binary tree schannel
 #vanakkam ippo namba binary tree pathi paaka porom
+# binary tree
+#complete binary tree 
+#full binary tree
+#half binary tree
+#Degenerate Binary Tree
+#Skewed Binary Trees
